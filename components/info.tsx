7a0448cd3b00b0ef4c435428954deb1f13d2ba8f
muafalah <muh.ifa.amrillah@gmail.com>
2023-08-07 17:12:30 +0700

    fix bug add to cart

--- components/info.tsx
+++ components/info.tsx
@@ -5,12 +5,19 @@ import { ShoppingCart } from "lucide-react";
 import { Product } from "@/types";
 import Currency from "@/components/product/currency";
 import Button from "@/components/button/button";
+import useCart from "@/hooks/use-cart";
 
 interface InfoProps {
   data: Product;
 }
 
 const Info = ({ data }: InfoProps) => {
+  const cart = useCart();
+
+  const onAddToCart = () => {
+    cart.addItem(data);
+  };
+
   return (
     <div>
       <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
@@ -34,7 +41,7 @@ const Info = ({ data }: InfoProps) => {
         </div>
       </div>
       <div className="mt-10 flex items-center gap-x-3">
-        <Button className="flex items-center gap-x-2">
+        <Button onClick={onAddToCart} className="flex items-center gap-x-2">
           Add To Cart
           <ShoppingCart />
         </Button>
